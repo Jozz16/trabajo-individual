ab552cfb3e4a258fbcab6b9b0c74297f19c55689
Jozz16 <jo633554@gmail.com>
2023-01-19 21:45:44 -0300

    registro

---
+++ js/registro.js
@@ -0,0 +1,6 @@
+// variables
+let Nombre = document.getElementById("inputNombre")
+let correo = document.getElementById("inputEmail")
+let contraseña = document.getElementById("inputContraseña")
+
+
