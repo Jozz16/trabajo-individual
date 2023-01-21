ca7d69d318ebe0f1c468897879eec0d9250d7fb1
Jozz16 <jo633554@gmail.com>
2023-01-20 21:24:08 -0300

    validar

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,13 @@
 // variables
-let Nombre = document.getElementById("inputNombre")
+let nombre = document.getElementById("inputNombre")
 let correo = document.getElementById("inputEmail")
 let contraseña = document.getElementById("inputContraseña")
 
 
+
+function enviar(){
+if(nombre.value == ""){
+
+}
+
+}
