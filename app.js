861b757da5c7d0a0346710061971cc5d27cc0be3
Jozz16 <jo633554@gmail.com>
2023-04-19 13:40:16 -0400

    login completo

--- app.js
+++ app.js
@@ -1,45 +1,84 @@
-const express = require('express')
-const { Pool } = require("pg")
+const express = require("express");
+const { Pool } = require("pg");
 
-const app = express()
-const puerto=3001;
-const hbs = require('hbs')
-const path = require('path');
-const { METHODS } = require('http');
-const { json } = require('sequelize');
+const app = express();
+const puerto = 3001;
+const hbs = require("hbs");
+const path = require("path");
+const { METHODS } = require("http");
+const { json } = require("sequelize");
 
-hbs.registerPartials( __dirname + '/views/partials')
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.set('view engine', 'hbs')
-app.set('views', './views')
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
 //midlawares
-app.use('/js', express.static('/js'))
-app.use(express.static( path.join( __dirname , 'node_modules/bootstrap/dist')))
-app.use(express.static( path.join( __dirname , 'public')))
+app.use("/js", express.static("/js"));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// static
-app.get("/",(req,res)=>{
-   res.render("index")
+// Ruta para mostrar la página principal
+app.get("/", (req, res) => {
+  res.render("index");
 });
-app.get("/contactanos",(req,res)=>{
-   res.render("contacto")
-});
-app.get("/encuentra",(req,res)=>{
-   res.render("encuentra")
-});
-app.get("/regala",(req,res)=>{
-   res.render("regala")
-});
-app.get("/registrate",(req,res)=>{
 
-   res.render("registrate")
+// Ruta para mostrar la página de contacto
+app.get("/contactanos", (req, res) => {
+  res.render("contacto");
 });
-app.post("/registrate", async (req,res)=>{
 
-   const { name, email, password } = req.body;
+// Ruta para mostrar la página de búsqueda de productos
+app.get("/encuentra", (req, res) => {
+  res.render("encuentra");
+});
+
+// Ruta para mostrar la página de regalos
+app.get("/regala", (req, res) => {
+  res.render("regala");
+});
+
+// Ruta para mostrar la página de inicio de sesión
+app.get("/iniciar-sesion", (req, res) => {
+  res.render("inicio-sesion");
+});
+
+// Ruta para procesar la petición de inicio de sesión
+app.post("/iniciar-sesion", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const response = await fetch("http://localhost:3002/login/v1", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    if (response.ok) {
+      console.log(`El usuario con email ${email} se ha logueado`);
+      res.status(200).send(`El usuario con email ${email} se ha logueado`);
+    } else {
+      console.error("Error al iniciar sesión");
+      res.status(500).send("Error al iniciar sesión");
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send(error.message);
+  }
+});
+
+// Ruta para mostrar la página de registro de usuario
+app.get("/registrate", (req, res) => {
+  res.render("registrate");
+});
+
+// Ruta para procesar la petición de registro de usuario
+app.post("/registrate", async (req, res) => {
+  const { name, email, password } = req.body;
   try {
     const response = await fetch("http://localhost:3002/register/v1", {
       method: "POST",
@@ -64,19 +103,12 @@ app.post("/registrate", async (req,res)=>{
   }
 });
 
-// plantilla abajo
-app.all("*",(req,res)=>{
-    res.status(404).send("pagina no existe")
-})
+// Ruta para manejar las peticiones de cualquier otra página que no existe
+app.all("*", (req, res) => {
+  res.status(404).send("pagina no existe");
+});
+
 //plantilla abajo
-app.listen(puerto,()=>{
-    console.log("servicio levantado")
-})
-
-
-
-
-
-
-
-
+app.listen(puerto, () => {
+  console.log("servicio levantado");
+});
--- public/js/registro.js
+++ public/js/registro.js
@@ -1,66 +1,61 @@
-// Declaracion de variables a usar
 const nombreInput = document.getElementById('nombre');
 const contraseñaInput = document.getElementById('contraseña');
 const emailInput = document.querySelector('#email');
-const formulario = document.querySelector('.formulario')
-
+const formulario = document.querySelector('.formulario');
 
 const datos = {
-    nombre:'',
-    email:'', 
-    contraseña:''
-}
+  nombre:'',
+  email:'', 
+  contraseña:''
+};
 
-nombreInput.addEventListener('input', leerTexto)
-emailInput.addEventListener('input', leerTexto)
-contraseñaInput.addEventListener('input', leerTexto)
+nombreInput.addEventListener('input', leerTexto);
+emailInput.addEventListener('input', leerTexto);
+contraseñaInput.addEventListener('input', leerTexto);
 
-// Evento submit 
 formulario.addEventListener('submit', function(e){
-    // e.preventDefault()
-    
-    // Validar el formulario
+  e.preventDefault();
+  
+  const {nombre, email, contraseña} = datos;
 
-    const {nombre, email, contraseña} = datos
-    if(nombre == '' || email == ''||contraseña== ''){
-        mostrarError('Todos los campos son obligatorios*')
-        return
-    }
-    mostrarAprobacion('Formulario enviado correctamente')
-    limpiarFormulario()
-})
+  if(nombre == '' || email == ''||contraseña== ''){
+    mostrarError('Todos los campos son obligatorios*');
+    return;
+  }
+
+  mostrarAprobacion('Formulario enviado correctamente');
+  limpiarFormulario();
+});
 
 function mostrarError(mensaje){
-    let error = document.createElement('P')
-    error.textContent = mensaje
-   
-    error.classList.add('error')
-    formulario.appendChild(error)
-    //ocultar alerta
-    setTimeout(() => {
-        error.remove()
-    }, 4000);
-    
+  let error = document.createElement('P');
+  error.textContent = mensaje;
+ 
+  error.classList.add('error');
+  formulario.appendChild(error);
+
+  setTimeout(() => {
+    error.remove();
+  }, 4000);
 }
+
 function mostrarAprobacion(mensaje){
-    let check = document.createElement('P')
-    check.textContent = mensaje
-    check.classList.add('check')
-    formulario.appendChild(check)
-    //ocultar check
-    setTimeout(() => {
-        check.remove()
-    }, 5000);
+  let check = document.createElement('P');
+  check.textContent = mensaje;
+  check.classList.add('check');
+  formulario.appendChild(check);
+
+  setTimeout(() => {
+    check.remove();
+  }, 5000);
 }
 
 function limpiarFormulario(){
-    nombreInput.value =''
-    emailInput.value =''
-    mensajeInput.value =''
+  nombreInput.value ='';
+  emailInput.value ='';
+  contraseñaInput.value ='';
 }
 
 function leerTexto(e) {
-    
-    datos[e.target.id] = e.target.value
-    // console.log(datos)
+  datos[e.target.id] = e.target.value;
 }
